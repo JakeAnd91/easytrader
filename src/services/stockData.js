7e4a0f5c9e9b8b2222ea6a5db0fe1f9cc592a236
ethananders76 <ethanmanderson76@gmail.com>
2026-08-08 22:09:52 -0600

    Build multi-provider stock data service

--- src/services/stockData.js
+++ src/services/stockData.js
@@ -12,3 +12,41 @@ export async function getQuote(symbol) {
 
   return data;
 }
+
+
+export async function getFMPQuote(symbol) {
+  const response = await fetch(
+    `https://financialmodelingprep.com/stable/quote?symbol=${symbol}&apikey=${FMP_KEY}`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
+
+export async function getNews(symbol) {
+  const response = await fetch(
+    `https://finnhub.io/api/v1/company-news?symbol=${symbol}&from=2026-08-01&to=2026-08-08&token=${FINNHUB_KEY}`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
+
+export async function getStockData(symbol) {
+  const [quote, fmpQuote, news] = await Promise.all([
+    getQuote(symbol),
+    getFMPQuote(symbol),
+    getNews(symbol),
+  ]);
+
+  return {
+    quote,
+    fmpQuote,
+    news,
+  };
+}
+
